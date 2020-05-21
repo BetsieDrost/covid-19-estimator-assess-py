3bce7f0540c8b2ee785e0dad5e288e5470a3855f
BetsieDrost <betsie.drost@gmail.com>
2020-05-21 02:45:32 +0200

    On branch assessment Changes to be committed: 	modified:   src/estimator.py 	estimator function was added back 	data input functions and test data removed

--- src/estimator.py
+++ src/estimator.py
@@ -1,109 +1,106 @@
 #COVID-19 ESTIMATOR - CHALLENGE 1 - BUILDFORSDG2020 ASSESSMENT PHASE
+def estimator(data):
+    
+    impact_data = data
+        
+    # Assumptions for model
+    impactMultiplier = 10
+    severeImpactMultiplier = 50
+    casesRequiringHospitalisation = 0.15
+    casesRequiringICU = 0.05
+    casesRequiringVentilators= 0.02
+    hospitalBedCapacityAvailable = 0.35
+     
+    #Challenge 1: Determine Currently Infected and Infections by Requested Time
+    daysToElapse = (convert_period_to_days(impact_data.get("periodType"), \
+                impact_data.get("timeToElapse")))
+    casesReported = impact_data.get("reportedCases")
+    multiplierFactor = int(factor_infections_by_requested_time(daysToElapse))
+    
+    ch1ImpactCurrentlyInfected = casesReported * impactMultiplier  
+    ch1SevereImpactCurrentlyInfected = casesReported * severeImpactMultiplier
+    ch1ImpactInfectionsByRequestedTime = ch1ImpactCurrentlyInfected * multiplierFactor
+    ch1SevereImpactInfectionsByRequestedTime = ch1SevereImpactCurrentlyInfected * multiplierFactor
+    
+    #Challenge2: Determine Cases Requiring Hospitalisation by requested time and
+    # hospital beds available for COVID-19 patients
+    
+    allHospitalBeds = impact_data.get("totalHospitalBeds")
+    
+    ch2ImpactSevereCasesByRequestedTime = \
+        int(ch1ImpactInfectionsByRequestedTime * casesRequiringHospitalisation)
+        
+    ch2SevereImpactSevereCasesByRequestedTime = \
+        (ch1SevereImpactInfectionsByRequestedTime * casesRequiringHospitalisation )
+        
+    ch2BedsAvailableforCovid19 = \
+       allHospitalBeds * hospitalBedCapacityAvailable
+        
+    ch2ImpactOpenHospitalBeds = \
+        ch2BedsAvailableforCovid19 - ch2ImpactSevereCasesByRequestedTime
+    
+    ch2SevereImpactOpenHospitalBeds = \
+        (ch2BedsAvailableforCovid19 - ch2SevereImpactSevereCasesByRequestedTime)
+    
+    # Challenge 3 - Part 1:  Determine cases requiring ventilators and cases requiring ICU
+    
+    ch3ImpactICUCasesByInfectedTime = \
+        (ch1ImpactInfectionsByRequestedTime * casesRequiringICU)
+    ch3SevereImpactICUCasesByInfectedTime = \
+        (ch1SevereImpactInfectionsByRequestedTime * casesRequiringICU)
+    ch3ImpactVentilatorCasesByInfectedTime = \
+        (ch1ImpactInfectionsByRequestedTime * casesRequiringVentilators)
+    ch3SevereImpactVentilatorCasesByInfectedTime = \
+        (ch1SevereImpactInfectionsByRequestedTime * casesRequiringVentilators)
+    
+    #  Challenge 3 - Part 2 : Determine the DollarsInFlight
+    
+    avgDailyDollars = impact_data["region"]["avgDailyIncomeInUSD"]
+    popEarnAveDollars = impact_data["region"]["avgDailyIncomePopulation"]
+    ch3ImpactDollarsInFlight = \
+        int(avgDailyDollars * popEarnAveDollars * ch1ImpactInfectionsByRequestedTime)
+    ch3SevereImpactDollarsInFlight = \
+        int(avgDailyDollars * popEarnAveDollars * ch1ImpactInfectionsByRequestedTime)
+    
+    #Output 
+    output = {
+            "data" : impact_data, 
+            "impact": {
+                    "currentlyInfected" : ch1ImpactCurrentlyInfected,
+                    "infectionsByRequestedTime" : ch1ImpactInfectionsByRequestedTime,
+                    "severeCasesByRequestedTime" : ch2ImpactSevereCasesByRequestedTime,
+                    "hospitalBedsByRequestedTime" : ch2ImpactOpenHospitalBeds,
+                    "casesForICUByRequestedTime"  : ch3ImpactICUCasesByInfectedTime,
+                    "casesForVentilatorsByRequestedTime" : ch3ImpactVentilatorCasesByInfectedTime,
+                    "dollarsInFlight": ch3ImpactDollarsInFlight
+                    },
+            "severeImpact": {
+                    "currentlyInfected" : ch1SevereImpactCurrentlyInfected,
+                    "infectionsByRequestedTime" : ch1SevereImpactInfectionsByRequestedTime,
+                    "severeCasesByRequestedTime" : ch2SevereImpactSevereCasesByRequestedTime,
+                    "hospitalBedsByRequestedTime" : ch2SevereImpactOpenHospitalBeds,
+                    "casesForICUByRequestedTime"  : ch3SevereImpactICUCasesByInfectedTime,
+                    "casesForVentilatorsByRequestedTime" : ch3SevereImpactVentilatorCasesByInfectedTime,
+                    "dollarsInFlight": ch3ImpactDollarsInFlight
+                    }
+            }
+    return output
 
-
-
-def model_inputs():
-    reviewRegion= region_input()
-    x = reviewRegion.get("regionName")
-    currentlyInfected = int(input(f"How many Covid-19 cases are currently reported for {x}? "))
-    periodTypeInput=convert_period_to_days()
-    typePeriod=periodTypeInput[0]
-    dayConvertor=periodTypeInput[1]
-    typePeriodToElapse=int(input(f" For how many {typePeriod} elapsed would you like the estimation to be done? "))
-    convertedDaysToElapse = typePeriodToElapse * dayConvertor
-    regionHospitalBeds = input(f" What is the total number of hospital beds in {x}? ")
-    multiplierFactor = infections_by_requested_time(convertedDaysToElapse)
-    fixed_data = {
-                   "regionName:": x,
-                   "periodType" : typePeriod,
-                   "timeToElapse" : typePeriodToElapse,
-                   "daysToElapse" : convertedDaysToElapse,
-                   "totalHospitalBeds" : regionHospitalBeds,
-                   "reportedCases" : currentlyInfected,
-                   "factorForInfections" : multiplierFactor,
-                   "impactMultiplier" : 10,
-                   "severeImpactMultiplier" : 50,
-                   "casesRequiringHospitalisation" : 0.15,
-                   "casesRequiringVentilators": 0.05,
-                   "casesRequiringVentilators": 0.02,
-                   "hopitalBedCapacityAvailable": 0.35,
-                }
-    return fixed_data
-
-
-def region_input():
-    regionName = input("Enter the region: ")
-    regionAveAge = float(input(f"Average age of the population of {regionName}? "))
-    regionPopulation = int(input(f"What is the population of {regionName}? "))
-    regionAveDailyIncome = int(input(f"What is the average daily income per person in USD for {regionName}? "))
-    regionAveDailyIncomePercentPopulation =  float(input(f"What percentage (enter as decimal) of the population  of {regionName} earns the average daily income of USD {regionAveDailyIncome}? "))
-    region = {  
-               "regionName" : regionName,
-               "avgAge": regionAveAge,
-               "avgDailyIncomeUSD": regionAveDailyIncome,
-               "avgDailyIncomePopulation": regionAveDailyIncomePercentPopulation, 
-               "population": regionPopulation
-              }
-    return region   
-
-
-def infections_by_requested_time(daysToElapse):
+def factor_infections_by_requested_time(daysToElapse):
     daysInCylcle = daysToElapse
     daysInfectionsDouble = 3 
     powerFactor = (int(daysToElapse / daysInfectionsDouble))
     factor = 2 ** powerFactor
     return factor
     
-
     
-def convert_period_to_days():
-    periodTypes = ["days","months","weeks"]
-    test=False
-    while test == False:
-        periodType = (input(f"Select the period type you will use: {periodTypes[0:3]}")).lower()
-        if periodType == "days":
-            dayConvertor=1
-            test=True
-        elif periodType == "weeks":
-            dayConvertor=7
-            test=True
-        elif periodType == "months":
-            dayConvertor=30
-            test=True
-        else: print("Invalid input")
-    return periodType, dayConvertor
+def convert_period_to_days(periodType,timeToElapse):
+    if periodType.lower() == "days":
+        dayConvertor=1
+    elif periodType.lower() == "weeks":
+        dayConvertor=7
+    elif periodType.lower() == "months":
+        dayConvertor=30
+    timeInDays = timeToElapse * dayConvertor    
+    return timeInDays
 
-
-  
-def impact_estimator(data):
-    impact_data=data
-    multiplier = impact_data.get("impactMultiplier")
-    factor = impact_data.get("factorForInfections")
-    currentInfections = impact_data.get("reportedCases")
-    estimateInfectionsByRequestedTime = currentInfections * factor * multiplier
-    impact = {
-                "infectionsByRequestedTime" : estimateInfectionsByRequestedTime
-             }
-    return impact
-
-
-def severe_impact_estimator(data):
-    severe_impact_data=data
-    multiplier = severe_impact_data.get("severeImpactMultiplier")
-    factor = severe_impact_data.get("factorForInfections")
-    currentInfections = severe_impact_data.get("reportedCases")
-    estimateSevereInfectionsByRequestedTime = currentInfections * factor * multiplier
-    severeImpact = {
-                "infectionsByRequestedTime" : estimateSevereInfectionsByRequestedTime
-             }
-    return severeImpact
-    
-    
-    
-
-myResult = model_inputs()
-print(f"This is my input data: {myResult}")
-myImpactResult = impact_estimator(myResult)
-print(f"This is my Impact output: {myImpactResult}")
-mySevereImpactResult = severe_impact_estimator(myResult)
-print(f"This is my Severe Impact output: {mySevereImpactResult}")
